5337640c9f8e686e0ebaef862ac32fccd253425d
Julian Ricardo <jric376@users.noreply.github.com>
2020-04-04 10:51:59 -0400

    Initial guess (two) commit; set factors for gender, ethnicity

--- R/load_guess.R
+++ R/load_guess.R
@@ -18,12 +18,16 @@ ag_gamers <- here::here("data", "ag_gamers.csv") %>%
   fread() %>%
   as.data.table() %>%
   .[, ':=' (access = anytime::anytime(access),
-            created = anytime::anytime(created))]
+            created = anytime::anytime(created))] %>%
+  .[, gender := fct_recode(gender, "Not specified" = "")] %>%
+  .[, ethnicity := fct_recode(ethnicity, "Not specified" = "")]
 
 ag_photos <- here::here("data", "ag_photos.csv") %>%
   fread() %>%
   as.data.table() %>%
-  .[, created := anytime::anytime(created)]
+  .[, created := anytime::anytime(created)] %>%
+  .[, gender := fct_recode(gender, "Not specified" = "")] %>%
+  .[, ethnicity := fct_recode(ethnicity, "Not specified" = "")]
 
 ag_quality <- here::here("data", "ag_quality.csv") %>%
   fread() %>%
@@ -34,3 +38,10 @@ ag_report <- here::here("data", "ag_report.csv") %>%
   fread() %>%
   as.data.table() %>%
   .[, created := anytime::anytime(created)]
+
+ag_guess_ethn_match <- ag_guess[unique(ag_gamers[, c("uid", "ethnicity")]),
+                                on = "uid", nomatch = 0] %>%
+  merge(ag_photos[, c("photo_id", "ethnicity")] %>%
+          setnames("ethnicity", "photo_ethnicity"),
+        by = "photo_id") %>%
+  .[, match_ethnicity := ethnicity == photo_ethnicity]
